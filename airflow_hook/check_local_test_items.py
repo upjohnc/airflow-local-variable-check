3189a14f9122fa4783da84d8bea9ba966b8133fc
Chad Upjohn <cupjohn@condati.com>
2019-04-04 12:06:36 -0500

    change to using repo values

--- airflow_hook/check_local_test_items.py
+++ airflow_hook/check_local_test_items.py
@@ -1,23 +1,27 @@
 import re
+import subprocess
 from pathlib import Path
 
 from loguru import logger
 
-dir_ = 'ESET_Renewal'
-
 
 def main():
-    my_dir = Path.home() / 'projects' / 'Airflow' / 'dags' / dir_
-    for i in my_dir.glob('*.py'):
-        with i.open() as f:
+    root_dir = subprocess.Popen('git rev-parse --show-toplevel'.split(), stdout=subprocess.PIPE).communicate()
+    path_root = Path(root_dir[0].decode().replace('\n', ''))
+
+    file_names = subprocess.Popen('git diff development --name-only'.split(), stdout=subprocess.PIPE).communicate()
+    for i in [i for i in file_names[0].decode().splitlines() if i.endswith('py') and i.startswith('dag')]:
+        file_path = path_root / i
+
+        with file_path.open() as f:
             contents_ = f.read()
         if re.search(r'\btest\b|.*_test.*|.*test_.*', contents_, re.IGNORECASE):
-            logger.info('Test in : {}'.format(i.resolve()))
+            logger.info('Test in : {}'.format(file_path.resolve()))
         email_address = 'cupjohn@condati.com'
         if re.search(r'{}'.format(email_address), contents_, re.IGNORECASE):
-            logger.info('Bad Email address {}  : {}'.format(email_address, i.resolve()))
+            logger.info('Bad Email address {}  : {}'.format(email_address, file_path.resolve()))
 
-    test_dir = Path.home() / 'projects' / 'Airflow' / 'tests'
+    test_dir = path_root / 'tests'
     for i in test_dir.glob('**/*.py'):
         with i.open() as f:
             contents_ = f.read()
