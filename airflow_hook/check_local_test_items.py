0a589d90b5a7dfcbdae3ae4d9fac38b4bc20d86f
Chad Upjohn <cupjohn@condati.com>
2019-04-04 07:44:17 -0500

    check for assert in tests

--- airflow_hook/check_local_test_items.py
+++ airflow_hook/check_local_test_items.py
@@ -17,6 +17,18 @@ def main():
         if re.search(r'{}'.format(email_address), contents_, re.IGNORECASE):
             logger.info('Bad Email address {}  : {}'.format(email_address, i.resolve()))
 
+    # todo: do recursive check
+    # todo: add test
+    test_dir = Path.home() / 'projects' / 'Airflow' / 'tests' / 'tests_ad_roll'
+    for i in test_dir.glob('*.py'):
+        with i.open() as f:
+            contents_ = f.read()
+        content_by_line = contents_.splitlines()
+        for line_number, line in enumerate(content_by_line):
+            if re.search(r'\bwith.*\.connect.*', line, re.IGNORECASE):
+                if not re.search(r'\bassert.*', content_by_line[line_number + 1]):
+                    logger.info('missing assert statement after with connect: line number {}, {}, {}'.format(line_number, line, i))
+
 
 if __name__ == '__main__':
     main()
---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='airflow_check_local_variables',
+    version='1.0.0',
+    packages=[''],
+    url='',
+    license='',
+    author='charlesupjohn',
+    author_email='',
+    description=''
+)
