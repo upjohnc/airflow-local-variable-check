2ca47ee8285eddf13e7263ce3827cf9446514896
Chad Upjohn <cupjohn@condati.com>
2019-04-05 07:23:08 -0500

    adding exit values

--- airflow_hook/check_local_test_items.py
+++ airflow_hook/check_local_test_items.py
@@ -20,6 +20,7 @@ def main():
     root_dir = subprocess.Popen('git rev-parse --show-toplevel'.split(), stdout=subprocess.PIPE).communicate()
     path_root = Path(root_dir[0].decode().replace('\n', ''))
 
+    all_good = True
     file_names = subprocess.Popen('git diff development --name-only'.split(), stdout=subprocess.PIPE).communicate()
     for i in [i for i in file_names[0].decode().splitlines() if i.endswith('py') and i.startswith('dag')]:
         file_path = path_root / i
@@ -27,9 +28,11 @@ def main():
         with file_path.open() as f:
             contents_ = f.read()
         if re.search(r'\btest\b|.*_test.*|.*test_.*', contents_, re.IGNORECASE):
+            all_good = False
             print('Test in : {}'.format(file_path.resolve()))
         email_address = 'cupjohn@condati.com'
         if re.search(r'{}'.format(email_address), contents_, re.IGNORECASE):
+            all_good = False
             print('Bad Email address {}  : {}'.format(email_address, file_path.resolve()))
 
     test_dir = path_root / 'tests'
@@ -39,6 +42,10 @@ def main():
         for file_name, lines in file.items():
             print(file_name)
             print('lines: ', [i['line_number'] for i in lines])
+            if file_name:
+                all_good = False
+
+    return 0 if all_good else 1
 
 
 if __name__ == '__main__':
